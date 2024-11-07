6ae688eb1d86e7b282a7cb878b723c245ce487f7
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-11-07 00:15:53 -0500

    Add temporary class

---
+++ DataLayer/Prueba.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace DataLayer
+{
+    public class Prueba
+    {
+        //public static string CualquierCosa()
+        //{
+        //    string connectionString = @"Data Source=JULIAN;Initial Catalog=GestionVeterinarias;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
+
+        //    SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+        //    sqlConnection.Open();
+
+        //    string query = "SELECT * FROM Administrador";
+        //    SqlCommand command = new SqlCommand(query, sqlConnection);
+
+        //    SqlDataReader reader = command.ExecuteReader();
+
+        //    return "Consulta exitosa";
+        //}
+    }
+}
--- GestionVeterinarias/Inicio.Designer.cs
+++ GestionVeterinarias/Inicio.Designer.cs
@@ -38,39 +38,45 @@
             this.txtUsuario = new System.Windows.Forms.TextBox();
             this.lblClave = new System.Windows.Forms.Label();
             this.lblUsuario = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.gbLogin.SuspendLayout();
             this.gbRol.SuspendLayout();
             this.SuspendLayout();
             // 
             // gbLogin
             // 
+            this.gbLogin.Controls.Add(this.label1);
             this.gbLogin.Controls.Add(this.btnEntrar);
             this.gbLogin.Controls.Add(this.txtClave);
             this.gbLogin.Controls.Add(this.gbRol);
             this.gbLogin.Controls.Add(this.txtUsuario);
             this.gbLogin.Controls.Add(this.lblClave);
             this.gbLogin.Controls.Add(this.lblUsuario);
-            this.gbLogin.Location = new System.Drawing.Point(12, 12);
+            this.gbLogin.Location = new System.Drawing.Point(8, 8);
+            this.gbLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gbLogin.Name = "gbLogin";
-            this.gbLogin.Size = new System.Drawing.Size(563, 402);
+            this.gbLogin.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gbLogin.Size = new System.Drawing.Size(375, 261);
             this.gbLogin.TabIndex = 0;
             this.gbLogin.TabStop = false;
             this.gbLogin.Text = "Iniciar sesión";
             // 
             // btnEntrar
             // 
-            this.btnEntrar.Location = new System.Drawing.Point(199, 298);
+            this.btnEntrar.Location = new System.Drawing.Point(133, 194);
+            this.btnEntrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEntrar.Name = "btnEntrar";
-            this.btnEntrar.Size = new System.Drawing.Size(147, 67);
+            this.btnEntrar.Size = new System.Drawing.Size(98, 44);
             this.btnEntrar.TabIndex = 8;
             this.btnEntrar.Text = "ENTRAR";
             this.btnEntrar.UseVisualStyleBackColor = true;
             // 
             // txtClave
             // 
-            this.txtClave.Location = new System.Drawing.Point(215, 106);
+            this.txtClave.Location = new System.Drawing.Point(143, 69);
+            this.txtClave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtClave.Name = "txtClave";
-            this.txtClave.Size = new System.Drawing.Size(161, 26);
+            this.txtClave.Size = new System.Drawing.Size(109, 20);
             this.txtClave.TabIndex = 7;
             // 
             // gbRol
@@ -78,9 +84,11 @@
             this.gbRol.Controls.Add(this.rbVeterinario);
             this.gbRol.Controls.Add(this.rbRecepcionista);
             this.gbRol.Controls.Add(this.rbAdministrador);
-            this.gbRol.Location = new System.Drawing.Point(34, 162);
+            this.gbRol.Location = new System.Drawing.Point(23, 105);
+            this.gbRol.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gbRol.Name = "gbRol";
-            this.gbRol.Size = new System.Drawing.Size(480, 104);
+            this.gbRol.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gbRol.Size = new System.Drawing.Size(320, 68);
             this.gbRol.TabIndex = 3;
             this.gbRol.TabStop = false;
             this.gbRol.Text = "Rol";
@@ -88,9 +96,10 @@
             // rbVeterinario
             // 
             this.rbVeterinario.AutoSize = true;
-            this.rbVeterinario.Location = new System.Drawing.Point(181, 44);
+            this.rbVeterinario.Location = new System.Drawing.Point(121, 29);
+            this.rbVeterinario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rbVeterinario.Name = "rbVeterinario";
-            this.rbVeterinario.Size = new System.Drawing.Size(111, 24);
+            this.rbVeterinario.Size = new System.Drawing.Size(75, 17);
             this.rbVeterinario.TabIndex = 1;
             this.rbVeterinario.TabStop = true;
             this.rbVeterinario.Text = "Veterinario";
@@ -99,9 +108,10 @@
             // rbRecepcionista
             // 
             this.rbRecepcionista.AutoSize = true;
-            this.rbRecepcionista.Location = new System.Drawing.Point(298, 44);
+            this.rbRecepcionista.Location = new System.Drawing.Point(199, 29);
+            this.rbRecepcionista.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rbRecepcionista.Name = "rbRecepcionista";
-            this.rbRecepcionista.Size = new System.Drawing.Size(135, 24);
+            this.rbRecepcionista.Size = new System.Drawing.Size(93, 17);
             this.rbRecepcionista.TabIndex = 2;
             this.rbRecepcionista.TabStop = true;
             this.rbRecepcionista.Text = "Recepcionista";
@@ -110,9 +120,10 @@
             // rbAdministrador
             // 
             this.rbAdministrador.AutoSize = true;
-            this.rbAdministrador.Location = new System.Drawing.Point(43, 44);
+            this.rbAdministrador.Location = new System.Drawing.Point(29, 29);
+            this.rbAdministrador.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rbAdministrador.Name = "rbAdministrador";
-            this.rbAdministrador.Size = new System.Drawing.Size(132, 24);
+            this.rbAdministrador.Size = new System.Drawing.Size(88, 17);
             this.rbAdministrador.TabIndex = 0;
             this.rbAdministrador.TabStop = true;
             this.rbAdministrador.Text = "Administrador";
@@ -120,35 +131,48 @@
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(215, 63);
+            this.txtUsuario.Location = new System.Drawing.Point(143, 41);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(161, 26);
+            this.txtUsuario.Size = new System.Drawing.Size(109, 20);
             this.txtUsuario.TabIndex = 6;
             // 
             // lblClave
             // 
             this.lblClave.AutoSize = true;
-            this.lblClave.Location = new System.Drawing.Point(107, 109);
+            this.lblClave.Location = new System.Drawing.Point(71, 71);
+            this.lblClave.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblClave.Name = "lblClave";
-            this.lblClave.Size = new System.Drawing.Size(48, 20);
+            this.lblClave.Size = new System.Drawing.Size(34, 13);
             this.lblClave.TabIndex = 5;
             this.lblClave.Text = "Clave";
             // 
             // lblUsuario
             // 
             this.lblUsuario.AutoSize = true;
-            this.lblUsuario.Location = new System.Drawing.Point(107, 66);
+            this.lblUsuario.Location = new System.Drawing.Point(71, 43);
+            this.lblUsuario.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblUsuario.Name = "lblUsuario";
-            this.lblUsuario.Size = new System.Drawing.Size(64, 20);
+            this.lblUsuario.Size = new System.Drawing.Size(43, 13);
             this.lblUsuario.TabIndex = 4;
             this.lblUsuario.Text = "Usuario";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
             // Inicio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(585, 428);
+            this.ClientSize = new System.Drawing.Size(390, 278);
             this.Controls.Add(this.gbLogin);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Inicio";
             this.Text = "Inicio";
             this.gbLogin.ResumeLayout(false);
@@ -171,6 +195,7 @@
         private System.Windows.Forms.TextBox txtUsuario;
         private System.Windows.Forms.Label lblClave;
         private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label label1;
     }
 }
 

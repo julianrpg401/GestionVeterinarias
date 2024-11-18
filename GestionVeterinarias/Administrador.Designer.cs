bbb17585a5261a77729030e831e0f7b364836072
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-11-18 00:48:52 -0500

    Add events to display forms

--- GestionVeterinarias/Administrador.Designer.cs
+++ GestionVeterinarias/Administrador.Designer.cs
@@ -32,53 +32,75 @@
             this.btnRecepcionistas = new System.Windows.Forms.Button();
             this.btnPropietarios = new System.Windows.Forms.Button();
             this.btnMascotas = new System.Windows.Forms.Button();
+            this.btnCitas = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnVeterinarios
             // 
-            this.btnVeterinarios.Location = new System.Drawing.Point(12, 22);
+            this.btnVeterinarios.Location = new System.Drawing.Point(8, 14);
+            this.btnVeterinarios.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnVeterinarios.Name = "btnVeterinarios";
-            this.btnVeterinarios.Size = new System.Drawing.Size(185, 54);
+            this.btnVeterinarios.Size = new System.Drawing.Size(123, 35);
             this.btnVeterinarios.TabIndex = 0;
             this.btnVeterinarios.Text = "VETERINARIOS";
             this.btnVeterinarios.UseVisualStyleBackColor = true;
+            this.btnVeterinarios.Click += new System.EventHandler(this.btnVeterinarios_Click);
             // 
             // btnRecepcionistas
             // 
-            this.btnRecepcionistas.Location = new System.Drawing.Point(203, 22);
+            this.btnRecepcionistas.Location = new System.Drawing.Point(135, 14);
+            this.btnRecepcionistas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRecepcionistas.Name = "btnRecepcionistas";
-            this.btnRecepcionistas.Size = new System.Drawing.Size(185, 54);
+            this.btnRecepcionistas.Size = new System.Drawing.Size(123, 35);
             this.btnRecepcionistas.TabIndex = 1;
             this.btnRecepcionistas.Text = "RECEPCIONISTAS";
             this.btnRecepcionistas.UseVisualStyleBackColor = true;
+            this.btnRecepcionistas.Click += new System.EventHandler(this.btnRecepcionistas_Click);
             // 
             // btnPropietarios
             // 
-            this.btnPropietarios.Location = new System.Drawing.Point(12, 82);
+            this.btnPropietarios.Location = new System.Drawing.Point(8, 53);
+            this.btnPropietarios.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnPropietarios.Name = "btnPropietarios";
-            this.btnPropietarios.Size = new System.Drawing.Size(185, 54);
+            this.btnPropietarios.Size = new System.Drawing.Size(123, 35);
             this.btnPropietarios.TabIndex = 2;
             this.btnPropietarios.Text = "PROPIETARIOS";
             this.btnPropietarios.UseVisualStyleBackColor = true;
+            this.btnPropietarios.Click += new System.EventHandler(this.btnPropietarios_Click);
             // 
             // btnMascotas
             // 
-            this.btnMascotas.Location = new System.Drawing.Point(203, 82);
+            this.btnMascotas.Location = new System.Drawing.Point(135, 53);
+            this.btnMascotas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnMascotas.Name = "btnMascotas";
-            this.btnMascotas.Size = new System.Drawing.Size(185, 54);
+            this.btnMascotas.Size = new System.Drawing.Size(123, 35);
             this.btnMascotas.TabIndex = 3;
             this.btnMascotas.Text = "MASCOTAS";
             this.btnMascotas.UseVisualStyleBackColor = true;
+            this.btnMascotas.Click += new System.EventHandler(this.btnMascotas_Click);
+            // 
+            // btnCitas
+            // 
+            this.btnCitas.Location = new System.Drawing.Point(71, 92);
+            this.btnCitas.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCitas.Name = "btnCitas";
+            this.btnCitas.Size = new System.Drawing.Size(123, 35);
+            this.btnCitas.TabIndex = 4;
+            this.btnCitas.Text = "CITAS";
+            this.btnCitas.UseVisualStyleBackColor = true;
+            this.btnCitas.Click += new System.EventHandler(this.btnCitas_Click);
             // 
             // Administrador
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(401, 152);
+            this.ClientSize = new System.Drawing.Size(266, 132);
+            this.Controls.Add(this.btnCitas);
             this.Controls.Add(this.btnMascotas);
             this.Controls.Add(this.btnPropietarios);
             this.Controls.Add(this.btnRecepcionistas);
             this.Controls.Add(this.btnVeterinarios);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Administrador";
             this.Text = "Administrador";
             this.ResumeLayout(false);
@@ -91,5 +113,6 @@
         private System.Windows.Forms.Button btnRecepcionistas;
         private System.Windows.Forms.Button btnPropietarios;
         private System.Windows.Forms.Button btnMascotas;
+        private System.Windows.Forms.Button btnCitas;
     }
 }

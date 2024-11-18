386d4a09525f495448e60a332763e309dd3d2b63
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-11-18 00:48:33 -0500

    Add events to display forms

--- GestionVeterinarias/Administrador.cs
+++ GestionVeterinarias/Administrador.cs
@@ -15,8 +15,36 @@ namespace GestionVeterinarias
         public Administrador()
         {
             InitializeComponent();
+        }
 
-            
+        private void btnVeterinarios_Click(object sender, EventArgs e)
+        {
+            GestionVeterinarios gestionVeterinarios = new GestionVeterinarios();
+            gestionVeterinarios.ShowDialog();
+        }
+
+        private void btnRecepcionistas_Click(object sender, EventArgs e)
+        {
+            GestionRecepcionistas gestionRecepcionistas = new GestionRecepcionistas();
+            gestionRecepcionistas.ShowDialog();
+        }
+
+        private void btnPropietarios_Click(object sender, EventArgs e)
+        {
+            GestionPropietarios gestionPropietarios = new GestionPropietarios();
+            gestionPropietarios.ShowDialog();
+        }
+
+        private void btnMascotas_Click(object sender, EventArgs e)
+        {
+            GestionMascotas gestionMascotas = new GestionMascotas();
+            gestionMascotas.ShowDialog();
+        }
+
+        private void btnCitas_Click(object sender, EventArgs e)
+        {
+            GestionCitas gestionCitas = new GestionCitas();
+            gestionCitas.ShowDialog();
         }
     }
 }

3e201d573cbb51bb56bf0b370d67013a737a8fbc
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-11-18 20:56:50 -0500

    Edit form design

--- GestionVeterinarias/GestionMascotas.Designer.cs
+++ GestionVeterinarias/GestionMascotas.Designer.cs
@@ -44,36 +44,40 @@
             // 
             // btnRegistrar
             // 
-            this.btnRegistrar.Location = new System.Drawing.Point(23, 263);
+            this.btnRegistrar.Location = new System.Drawing.Point(49, 160);
+            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRegistrar.Name = "btnRegistrar";
-            this.btnRegistrar.Size = new System.Drawing.Size(134, 54);
+            this.btnRegistrar.Size = new System.Drawing.Size(89, 35);
             this.btnRegistrar.TabIndex = 0;
             this.btnRegistrar.Text = "REGISTRAR";
             this.btnRegistrar.UseVisualStyleBackColor = true;
             // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(439, 263);
+            this.btnEliminar.Location = new System.Drawing.Point(327, 160);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(134, 54);
+            this.btnEliminar.Size = new System.Drawing.Size(89, 35);
             this.btnEliminar.TabIndex = 1;
             this.btnEliminar.Text = "ELIMINAR";
             this.btnEliminar.UseVisualStyleBackColor = true;
             // 
             // btnConsultar
             // 
-            this.btnConsultar.Location = new System.Drawing.Point(300, 263);
+            this.btnConsultar.Location = new System.Drawing.Point(234, 160);
+            this.btnConsultar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnConsultar.Name = "btnConsultar";
-            this.btnConsultar.Size = new System.Drawing.Size(134, 54);
+            this.btnConsultar.Size = new System.Drawing.Size(89, 35);
             this.btnConsultar.TabIndex = 2;
             this.btnConsultar.Text = "CONSULTAR";
             this.btnConsultar.UseVisualStyleBackColor = true;
             // 
             // btnModificar
             // 
-            this.btnModificar.Location = new System.Drawing.Point(161, 263);
+            this.btnModificar.Location = new System.Drawing.Point(141, 160);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(134, 54);
+            this.btnModificar.Size = new System.Drawing.Size(89, 35);
             this.btnModificar.TabIndex = 3;
             this.btnModificar.Text = "MODIFICAR";
             this.btnModificar.UseVisualStyleBackColor = true;
@@ -81,14 +85,20 @@
             // gbDatosMascota
             // 
             this.gbDatosMascota.Controls.Add(this.txtEspecie);
+            this.gbDatosMascota.Controls.Add(this.btnModificar);
             this.gbDatosMascota.Controls.Add(this.lblNombreM);
+            this.gbDatosMascota.Controls.Add(this.btnConsultar);
+            this.gbDatosMascota.Controls.Add(this.btnEliminar);
             this.gbDatosMascota.Controls.Add(this.lblEspecie);
+            this.gbDatosMascota.Controls.Add(this.btnRegistrar);
             this.gbDatosMascota.Controls.Add(this.lblRaza);
             this.gbDatosMascota.Controls.Add(this.txtNombreM);
             this.gbDatosMascota.Controls.Add(this.txtRaza);
-            this.gbDatosMascota.Location = new System.Drawing.Point(89, 23);
+            this.gbDatosMascota.Location = new System.Drawing.Point(11, 11);
+            this.gbDatosMascota.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gbDatosMascota.Name = "gbDatosMascota";
-            this.gbDatosMascota.Size = new System.Drawing.Size(418, 218);
+            this.gbDatosMascota.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gbDatosMascota.Size = new System.Drawing.Size(466, 220);
             this.gbDatosMascota.TabIndex = 16;
             this.gbDatosMascota.TabStop = false;
             this.gbDatosMascota.Text = "Datos de la mascota";
@@ -96,62 +106,65 @@
             // txtEspecie
             // 
             this.txtEspecie.AcceptsTab = true;
-            this.txtEspecie.Location = new System.Drawing.Point(136, 94);
+            this.txtEspecie.Location = new System.Drawing.Point(188, 68);
+            this.txtEspecie.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtEspecie.Name = "txtEspecie";
-            this.txtEspecie.Size = new System.Drawing.Size(240, 26);
+            this.txtEspecie.Size = new System.Drawing.Size(161, 20);
             this.txtEspecie.TabIndex = 9;
             // 
             // lblNombreM
             // 
             this.lblNombreM.AutoSize = true;
-            this.lblNombreM.Location = new System.Drawing.Point(28, 65);
+            this.lblNombreM.Location = new System.Drawing.Point(116, 46);
+            this.lblNombreM.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblNombreM.Name = "lblNombreM";
-            this.lblNombreM.Size = new System.Drawing.Size(65, 20);
+            this.lblNombreM.Size = new System.Drawing.Size(44, 13);
             this.lblNombreM.TabIndex = 4;
             this.lblNombreM.Text = "Nombre";
             // 
             // lblEspecie
             // 
             this.lblEspecie.AutoSize = true;
-            this.lblEspecie.Location = new System.Drawing.Point(28, 97);
+            this.lblEspecie.Location = new System.Drawing.Point(116, 70);
+            this.lblEspecie.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblEspecie.Name = "lblEspecie";
-            this.lblEspecie.Size = new System.Drawing.Size(66, 20);
+            this.lblEspecie.Size = new System.Drawing.Size(45, 13);
             this.lblEspecie.TabIndex = 5;
             this.lblEspecie.Text = "Especie";
             // 
             // lblRaza
             // 
             this.lblRaza.AutoSize = true;
-            this.lblRaza.Location = new System.Drawing.Point(28, 129);
+            this.lblRaza.Location = new System.Drawing.Point(116, 94);
+            this.lblRaza.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblRaza.Name = "lblRaza";
-            this.lblRaza.Size = new System.Drawing.Size(47, 20);
+            this.lblRaza.Size = new System.Drawing.Size(32, 13);
             this.lblRaza.TabIndex = 7;
             this.lblRaza.Text = "Raza";
             // 
             // txtNombreM
             // 
-            this.txtNombreM.Location = new System.Drawing.Point(136, 62);
+            this.txtNombreM.Location = new System.Drawing.Point(188, 44);
+            this.txtNombreM.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtNombreM.Name = "txtNombreM";
-            this.txtNombreM.Size = new System.Drawing.Size(240, 26);
+            this.txtNombreM.Size = new System.Drawing.Size(161, 20);
             this.txtNombreM.TabIndex = 8;
             // 
             // txtRaza
             // 
-            this.txtRaza.Location = new System.Drawing.Point(136, 126);
+            this.txtRaza.Location = new System.Drawing.Point(188, 92);
+            this.txtRaza.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtRaza.Name = "txtRaza";
-            this.txtRaza.Size = new System.Drawing.Size(240, 26);
+            this.txtRaza.Size = new System.Drawing.Size(161, 20);
             this.txtRaza.TabIndex = 10;
             // 
             // GestionMascotas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 372);
+            this.ClientSize = new System.Drawing.Size(488, 242);
             this.Controls.Add(this.gbDatosMascota);
-            this.Controls.Add(this.btnModificar);
-            this.Controls.Add(this.btnConsultar);
-            this.Controls.Add(this.btnEliminar);
-            this.Controls.Add(this.btnRegistrar);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "GestionMascotas";
             this.Text = "Gestión de mascotas";
             this.gbDatosMascota.ResumeLayout(false);

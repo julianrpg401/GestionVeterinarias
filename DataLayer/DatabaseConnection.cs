64dd537d4f7527da63b1c9eedecb2b949dc0a439
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-11-20 01:13:00 -0500

    Change method to connect to the database

--- DataLayer/DatabaseConnection.cs
+++ DataLayer/DatabaseConnection.cs
@@ -15,12 +15,32 @@ namespace DataLayer
 {
     public class DatabaseConnection
     {
-        private readonly string _connectionString;
-        public string ConnectionString => _connectionString;
+        private static DatabaseConnection _instance;
+        private readonly SqlConnection _connection;
 
-        public DatabaseConnection()
+        private readonly string _connectionString;
+
+        private DatabaseConnection()
         {
             _connectionString = ConfigurationManager.ConnectionStrings["DbConnectionString"].ConnectionString;
+
+            _connection = new SqlConnection(_connectionString);
+        }
+
+        public static DatabaseConnection GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new DatabaseConnection();
+            }
+
+            return _instance;
+        }
+
+        //Método para obtener la conexión
+        public SqlConnection GetConnection()
+        {
+            return _connection;
         }
     }
 }

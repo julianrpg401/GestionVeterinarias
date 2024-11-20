660391262e8dd2439534384b332217f2d855f86a
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-11-20 01:10:56 -0500

    Add functionality: new user

--- BusinessLayer/EntityBusiness.cs
+++ BusinessLayer/EntityBusiness.cs
@@ -1,9 +1,11 @@
-﻿using EntityLayer;
+﻿using DataLayer;
+using EntityLayer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace BusinessLayer
 {
@@ -11,9 +13,15 @@ namespace BusinessLayer
     {
 
         // Método para agregar un nuevo veterinario
-        //public void RegistrarVeterinario(Veterinario veterinario)
-        //{
+        public void AgregarUsuario(string nombre, string especializacion, string horario, string email, string clave)
+        {
+            Veterinario veterinario = (Veterinario)UsuarioFactory.CrearUsuario(nombre, especializacion, horario, email, clave);
 
-        //}
+            using (var unitOfWork = new UnitOfWork())
+            {
+                unitOfWork.Usuario.Add(veterinario); // Agregar el empleado al repositorio
+                unitOfWork.Complete();
+            }
+        }
     }
 }
---
+++ BusinessLayer/UsuarioFactory.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusinessLayer
+{
+    public static class UsuarioFactory
+    {
+        public static Usuario CrearUsuario(string nombre, string especializacion, string horario, string email, string clave)
+        {
+            if (string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(especializacion) || string.IsNullOrEmpty(horario) || string.IsNullOrEmpty(clave))
+            {
+                throw new ArgumentException("Por favor, complete todos los campos.");
+            }
+
+            return new Veterinario(nombre, especializacion, horario, email, clave);
+        }
+    }
+}
